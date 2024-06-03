13ec2ba59b99526ab8b647d7c29b91e96591fb0c
Daniel Alejandro Alvarez Vanegas <96156811+DuneV@users.noreply.github.com>
2024-06-03 15:10:53 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define INTERNAL_LED 2
+
+void setup() {
+  pinMode(INTERNAL_LED,OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(INTERNAL_LED,HIGH);
+  Serial.println("LED ON");
+  delay(1000);
+  digitalWrite(INTERNAL_LED,LOW);
+  Serial.println("LED OFF");
+  delay(1000);
+}
